bae46871ca880c748b444c48c369e3b35e7b368f
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-03-11 01:39:10 -0800

    basic rendering for crystal

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,26 @@
-import { CrystalShard } from "./crystal";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { CrystalShard, Crystal } from "./crystal";
 const shard = new CrystalShard(7, "red");
 
-console.log(shard);
+class CrystalComponent extends React.Component<{ crystal: Crystal }, {}> {
+    public render() {
+        const energy = this.props.crystal.eval();
+        return (
+            <div className={"crystal crystal-" + energy.flavor}>
+                {energy.intensity}
+            </div>
+        );
+    }
+}
+
+
+const root = document.createElement("div");
+root.className = "root";
+document.body.appendChild(root);
+
+ReactDOM.render(
+    <CrystalComponent crystal={shard} />,
+    root
+);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: path.join(__dirname, "build", "src", "index.js"),
+    entry: path.join(__dirname, "build", "index.js"),
     output: {
         filename: "app.js"
     },
